015424d0250cd1623c37493eecdf4f34c13c8743
Sergio del Amo <sergio.delamo@softamo.com>
2019-02-05 15:47:51 +0100

    Validate state

--- src/main/java/io/micronaut/security/oauth2/handlers/DefaultAuthorizationResponseHandler.java
+++ src/main/java/io/micronaut/security/oauth2/handlers/DefaultAuthorizationResponseHandler.java
@@ -20,6 +20,8 @@ import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.client.RxHttpClient;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import io.micronaut.security.oauth2.openid.endpoints.authorization.InvalidStateException;
+import io.micronaut.security.oauth2.openid.endpoints.authorization.StateValidator;
 import io.micronaut.security.oauth2.openid.endpoints.token.AuthorizationCodeGrantRequestGenerator;
 import io.micronaut.security.oauth2.openid.idtoken.IdTokenAccessTokenResponse;
 import io.micronaut.security.oauth2.responses.AuthenticationResponse;
@@ -28,6 +30,7 @@ import io.reactivex.Single;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
 import javax.inject.Named;
 import javax.inject.Singleton;
 import java.util.Optional;
@@ -48,22 +51,31 @@ public class DefaultAuthorizationResponseHandler implements AuthorizationRespons
     private final IdTokenAccessTokenResponseHandler idTokenAccessTokenResponseHandler;
     private final RxHttpClient tokenClient;
 
+    private final @Nullable StateValidator stateValidator;
+
     /**
      * Creates a DefaultAuthorizationResponseHandler.
      * @param authorizationCodeGrantRequestGenerator Authorization Code Grant Request Generator
      * @param idTokenAccessTokenResponseHandler ID Token Access Token response handler
      * @param tokenClient RxHttpClient pointing to the token endpoint
+     * @param stateValidator Authorization state parameter validator
      */
     public DefaultAuthorizationResponseHandler(AuthorizationCodeGrantRequestGenerator authorizationCodeGrantRequestGenerator,
                                                IdTokenAccessTokenResponseHandler idTokenAccessTokenResponseHandler,
-                                               @Named("oauth2tokenendpoint") RxHttpClient tokenClient) {
+                                               @Named("oauth2tokenendpoint") RxHttpClient tokenClient,
+                                               @Nullable StateValidator stateValidator) {
         this.authorizationCodeGrantRequestGenerator = authorizationCodeGrantRequestGenerator;
         this.idTokenAccessTokenResponseHandler = idTokenAccessTokenResponseHandler;
         this.tokenClient = tokenClient;
+        this.stateValidator = stateValidator;
     }
 
     @Override
     public Single<HttpResponse<?>> handle(HttpRequest originalRequest, AuthenticationResponse authenticationResponse) {
+
+        if (stateValidator != null && authenticationResponse.getState() != null && !stateValidator.validate(originalRequest, authenticationResponse.getState())) {
+            throw new InvalidStateException("state " + authenticationResponse.getState() + " is not valid");
+        }
 
         HttpRequest request = authorizationCodeGrantRequestGenerator.generateRequest(authenticationResponse.getCode());
         try {
