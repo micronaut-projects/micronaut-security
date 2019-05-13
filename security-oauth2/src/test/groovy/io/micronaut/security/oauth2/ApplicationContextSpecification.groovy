5d0ba4fb5e31509794325029e063a5150b355729
Sergio del Amo <sergio.delamo@softamo.com>
2019-05-13 12:20:11 +0200

    AudienceClaimValidatorSpec

---
+++ security-oauth2/src/test/groovy/io/micronaut/security/oauth2/ApplicationContextSpecification.groovy
@@ -0,0 +1,54 @@
+package io.micronaut.security.oauth2
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.core.io.socket.SocketUtils
+import io.micronaut.runtime.server.EmbeddedServer
+import mock.OpenIdConfigurationController
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+abstract class ApplicationContextSpecification extends Specification {
+    @Shared
+    int mockOpenIdHttpServerPort = SocketUtils.findAvailableTcpPort()
+
+    @Shared
+    String mockOpenIdHttpServerUrl = "http://localhost:${mockOpenIdHttpServerPort}"
+
+    @Shared
+    Map<String, Object> openIdMockServerConf = [
+            "spec.name": "mockopenidprovider",
+            'micronaut.security.enabled': true,
+            'micronaut.security.token.jwt.enabled': true,
+            'micronaut.server.port': mockOpenIdHttpServerPort,
+            'mockserver.url': mockOpenIdHttpServerUrl,
+            'mockserver.path': '',
+            ]
+
+    @AutoCleanup
+    @Shared
+    EmbeddedServer openIdMockEmbeddedServer = ApplicationContext.run(EmbeddedServer, openIdMockServerConf)
+
+    @AutoCleanup
+    @Shared
+    ApplicationContext applicationContext = ApplicationContext.run(configuration)
+
+    String getIssuer() {
+        assert openIdMockEmbeddedServer.applicationContext.containsBean(OpenIdConfigurationController)
+        assert mockOpenIdHttpServerUrl != null
+        mockOpenIdHttpServerUrl
+    }
+
+    Map<String, Object> getConfiguration() {
+	            [
+        'micronaut.security.enabled': true,
+                'micronaut.security.token.jwt.enabled': true,
+                'micronaut.security.token.jwt.bearer.enabled': false,
+                'micronaut.security.token.jwt.cookie.enabled': true,
+                'micronaut.security.oauth2.enabled': true,
+                'micronaut.security.oauth2.clients.foo.client-id': 'XXXX',
+                'micronaut.security.oauth2.clients.foo.client-secret': 'YYYY',
+                'micronaut.security.oauth2.clients.foo.openid.issuer': getIssuer(),
+	            ]
+    }
+}
---
+++ security-oauth2/src/test/groovy/io/micronaut/security/oauth2/endpoint/token/response/validation/AudienceClaimValidatorSpec.groovy
@@ -0,0 +1,42 @@
+package io.micronaut.security.oauth2.endpoint.token.response.validation
+
+import io.micronaut.security.oauth2.ApplicationContextSpecification
+import io.micronaut.security.oauth2.client.OpenIdProviderMetadata
+import io.micronaut.security.oauth2.configuration.OauthClientConfiguration
+import io.micronaut.security.oauth2.endpoint.token.response.OpenIdClaims
+import spock.lang.Unroll
+
+class AudienceClaimValidatorSpec extends ApplicationContextSpecification {
+
+    @Unroll("#description")
+    void "audience claims validator"(String clientId,
+                                     List<String> audience,
+                                     String authorizedParty,
+                                     boolean expected,
+                                     String description) {
+        given:
+        applicationContext.containsBean(AudienceClaimValidator)
+        OauthClientConfiguration oauthClientConfiguration = Stub(OauthClientConfiguration) {
+            getClientId() >> clientId
+        }
+        OpenIdProviderMetadata openIdProviderMetadata = Mock(OpenIdProviderMetadata)
+        OpenIdClaims openIdClaims = Stub(OpenIdClaims) {
+            getAudience() >> audience
+            getAuthorizedParty() >> authorizedParty
+        }
+        AudienceClaimValidator validator = applicationContext.getBean(AudienceClaimValidator)
+
+        expect:
+        expected == validator.validate(openIdClaims, oauthClientConfiguration, openIdProviderMetadata)
+
+        where:
+        clientId   | audience            | authorizedParty || expected
+        'CLIENTID' | []                  | null            || false
+        'CLIENTID' | ['FOO']             | null            || false
+        'CLIENTID' | ['CLIENTID']        | null            || true
+        'CLIENTID' | ['CLIENTID', 'FOO'] | null            || false
+        'CLIENTID' | ['CLIENTID', 'FOO'] | 'YYY'           || true
+
+        description = audience.size() > 1 ? (authorizedParty ? "for multiple audiences, azp claim must be present." : "for multiple audiencies claim if azp is not present validation should fail") : (!audience ? 'if audience is empty validation fails' : (clientId != audience.first() ? 'if audience claim does not match client id validation must fail' : 'if audience claim must match client id'))
+    }
+}
