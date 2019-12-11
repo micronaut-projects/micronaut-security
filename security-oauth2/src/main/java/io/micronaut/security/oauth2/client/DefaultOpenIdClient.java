6a8360b6e5a6b6f973688a2abcb1b5b43ef27583
István <istvan.szoboszlai@inepex.com>
2019-12-11 11:10:36 +0100

    Forgotten import added

--- security-oauth2/src/main/java/io/micronaut/security/oauth2/client/DefaultOpenIdClient.java
+++ security-oauth2/src/main/java/io/micronaut/security/oauth2/client/DefaultOpenIdClient.java
@@ -40,6 +40,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
