94fa41d252cc72d1a4d368e0e2755a0f3ffddc8e
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-01-09 10:35:13 +0100

    Fixing tests

--- security-jwt/src/test/groovy/io/micronaut/docs/signandencrypt/MySignatureGeneratorConfigurationFactory.groovy
+++ security-jwt/src/test/groovy/io/micronaut/docs/signandencrypt/MySignatureGeneratorConfigurationFactory.groovy
@@ -17,12 +17,15 @@ package io.micronaut.docs.signandencrypt
 
 import io.micronaut.context.annotation.Bean
 import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Requires
+import io.micronaut.context.env.Environment
 import io.micronaut.security.token.jwt.signature.SignatureGeneratorConfiguration
 import io.micronaut.security.token.jwt.signature.rsa.RSASignatureGenerator
 import io.micronaut.security.token.jwt.signature.rsa.RSASignatureGeneratorConfiguration
 
 import javax.inject.Named
 
+@Requires(notEnv = Environment.TEST)
 //tag::clazz[]
 @Factory
 class MySignatureGeneratorConfigurationFactory {
