76f229968be8f65db4905119713ef05fc1a3f59c
jameskleeh <james.kleeh@gmail.com>
2020-02-29 16:55:15 -0500

    Fix invalid test

--- security/src/test/groovy/io/micronaut/docs/security/securityRule/intercepturlmap/BookController.groovy
+++ security/src/test/groovy/io/micronaut/docs/security/securityRule/intercepturlmap/BookController.groovy
@@ -18,6 +18,8 @@ package io.micronaut.docs.security.securityRule.intercepturlmap
 import io.micronaut.context.annotation.Requires
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Put
 
 @Requires(property = 'spec.name', value = 'docsintercepturlmap')
 @Controller('/books')
@@ -29,7 +31,17 @@ class BookController {
     }
 
     @Get('/grails')
-    String grails() {
+    String grails1() {
+        return "Grails Action"
+    }
+
+    @Put("/grails")
+    String grails2() {
+        return "Grails Action"
+    }
+
+    @Post("/grails")
+    String grails3() {
         return "Grails Action"
     }
 }
