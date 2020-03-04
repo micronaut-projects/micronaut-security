f15828338e8844936d756e12f07631de9f9f8898
jameskleeh <james.kleeh@gmail.com>
2020-03-03 22:04:31 -0500

    Refactor test

--- security-jwt/src/test/groovy/io/micronaut/security/token/propagation/TokenPropagationSpec.groovy
+++ security-jwt/src/test/groovy/io/micronaut/security/token/propagation/TokenPropagationSpec.groovy
@@ -35,138 +35,46 @@ import spock.lang.Stepwise
 
 import java.time.Duration
 
-@Stepwise
 class TokenPropagationSpec extends Specification {
 
     static final SPEC_NAME_PROPERTY = 'spec.name'
 
-    @Shared
-    int booksPort
-
-    @AutoCleanup
-    @Shared
-    EmbeddedServer booksEmbeddedServer
-
-    @AutoCleanup
-    @Shared
-    RxHttpClient booksClient
-
-    @Shared
-    int inventoryPort
-
-    @AutoCleanup
-    @Shared
-    EmbeddedServer inventoryEmbeddedServer
-
-    @AutoCleanup
-    @Shared
-    RxHttpClient inventoryClient
-
-    @AutoCleanup
-    @Shared
-    EmbeddedServer gatewayEmbeddedServer
-
-    @AutoCleanup
-    @Shared
-    RxHttpClient gatewayClient
-
-
-    def setupSpec() {
-
-    }
-
-    def "setup inventory server"() {
-        given:
-        inventoryPort = SocketUtils.findAvailableTcpPort()
+    void "test token propagation"() {
         Map inventoryConfig = [
                 'micronaut.application.name': 'inventory',
-                'micronaut.server.port'                       : inventoryPort,
                 (SPEC_NAME_PROPERTY)                          : 'tokenpropagation.inventory',
-                'micronaut.security.token.jwt.enabled'                 : true,
                 'micronaut.security.token.jwt.signatures.secret.validation.secret': 'pleaseChangeThisSecretForANewOne',
-                'micronaut.security.enabled'                  : true,
         ]
 
-        inventoryEmbeddedServer = ApplicationContext.run(EmbeddedServer, inventoryConfig, Environment.TEST)
+        EmbeddedServer inventoryEmbeddedServer = ApplicationContext.run(EmbeddedServer, inventoryConfig, Environment.TEST)
 
-        inventoryClient = inventoryEmbeddedServer.applicationContext.createBean(RxHttpClient, inventoryEmbeddedServer.getURL())
-
-        when:
-        for (Class beanClazz : [InventoryController]) {
-            inventoryEmbeddedServer.applicationContext.getBean(beanClazz)
-        }
-
-        then:
-        noExceptionThrown()
-    }
-
-    def "setup books server"() {
-        given:
-        booksPort = SocketUtils.findAvailableTcpPort()
         Map booksConfig = [
                 'micronaut.application.name': 'books',
-                'micronaut.server.port'                       : booksPort,
                 (SPEC_NAME_PROPERTY)                          : 'tokenpropagation.books',
-                'micronaut.security.token.jwt.enabled'                 : true,
                 'micronaut.security.token.jwt.signatures.secret.validation.secret': 'pleaseChangeThisSecretForANewOne',
-                'micronaut.security.enabled'                  : true,
         ]
 
-        booksEmbeddedServer = ApplicationContext.run(EmbeddedServer, booksConfig, Environment.TEST)
+        EmbeddedServer booksEmbeddedServer = ApplicationContext.run(EmbeddedServer, booksConfig, Environment.TEST)
 
-        booksClient = booksEmbeddedServer.applicationContext.createBean(RxHttpClient, booksEmbeddedServer.getURL())
-
-        when:
-        for (Class beanClazz : [BooksController]) {
-            booksEmbeddedServer.applicationContext.getBean(beanClazz)
-        }
-
-        then:
-        noExceptionThrown()
-    }
-
-    def "setup gateway server"() {
         given:
         Map gatewayConfig = [
                 (SPEC_NAME_PROPERTY): 'tokenpropagation.gateway',
                 'micronaut.application.name': 'gateway',
-                'micronaut.http.services.books.url': "http://localhost:${booksPort}",
-                'micronaut.http.services.inventory.url': "http://localhost:${inventoryPort}",
+                'micronaut.http.services.books.url': "${booksEmbeddedServer.getURL().toString()}",
+                'micronaut.http.services.inventory.url': "${inventoryEmbeddedServer.getURL().toString()}",
                 'micronaut.security.endpoints.login.enabled': true,
                 'micronaut.security.token.propagation.enabled': true,
                 'micronaut.security.token.propagation.service-id-regex': 'books|inventory',
-                'micronaut.security.token.jwt.enabled'                 : true,
                 'micronaut.security.token.jwt.signatures.secret.generator.secret': 'pleaseChangeThisSecretForANewOne',
                 'micronaut.security.token.writer.header.enabled': true,
         ]
 
-        gatewayEmbeddedServer = ApplicationContext.run(EmbeddedServer, gatewayConfig, Environment.TEST)
+        EmbeddedServer gatewayEmbeddedServer = ApplicationContext.run(EmbeddedServer, gatewayConfig, Environment.TEST)
 
-        when:
-        for (Class beanClazz : [
-                BooksClient,
-                InventoryClient,
-                GatewayController,
-                SampleAuthenticationProvider,
-                LoginController,
-        ]) {
-            gatewayEmbeddedServer.applicationContext.getBean(beanClazz)
-        }
-
-        then:
-        noExceptionThrown()
-
-
-        when:
         def configuration = new DefaultHttpClientConfiguration()
         configuration.setReadTimeout(Duration.ofSeconds(30))
-        gatewayClient = gatewayEmbeddedServer.applicationContext.createBean(RxHttpClient, gatewayEmbeddedServer.getURL(), configuration)
+        RxHttpClient gatewayClient = gatewayEmbeddedServer.applicationContext.createBean(RxHttpClient, gatewayEmbeddedServer.getURL(), configuration)
 
-        then:
-        noExceptionThrown()
-    }
-
-    def "verifies propagation works in a request which involves multiple services"() {
         when: 'attempt to login'
         def creds = new UsernamePasswordCredentials('sherlock', 'elementary')
         HttpResponse rsp = gatewayClient.toBlocking().exchange(HttpRequest.POST('/login', creds), BearerAccessRefreshToken)
