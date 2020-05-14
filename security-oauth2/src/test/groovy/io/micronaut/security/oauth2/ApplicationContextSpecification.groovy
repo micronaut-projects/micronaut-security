daf60283e381f56f919011b3b8c7b5d925c47aac
Sergio del Amo <sergio.delamo@softamo.com>
2020-05-14 11:05:51 +0200

    refactor: OpenIdMockEmbeddedServerSpecification
    Close: https://github.com/micronaut-projects/micronaut-security/issues/232

--- security-oauth2/src/test/groovy/io/micronaut/security/oauth2/ApplicationContextSpecification.groovy
+++ security-oauth2/src/test/groovy/io/micronaut/security/oauth2/ApplicationContextSpecification.groovy
@@ -1,59 +1,13 @@
 package io.micronaut.security.oauth2
 
 import io.micronaut.context.ApplicationContext
-import io.micronaut.core.io.socket.SocketUtils
-import io.micronaut.runtime.server.EmbeddedServer
-import mock.OpenIdConfigurationController
 import spock.lang.AutoCleanup
-import spock.lang.Retry
 import spock.lang.Shared
 import spock.lang.Specification
 
-@Retry(mode = Retry.Mode.SETUP_FEATURE_CLEANUP)
-abstract class ApplicationContextSpecification extends Specification {
+abstract class ApplicationContextSpecification extends Specification implements ConfigurationFixture {
 
-    int mockOpenIdHttpServerPort
-    String mockOpenIdHttpServerUrl
-    Map<String, Object> openIdMockServerConf
-    EmbeddedServer openIdMockEmbeddedServer
-    ApplicationContext applicationContext
-
-    String getMockServerPath() {
-        ''
-    }
-
-    String getIssuer() {
-        assert openIdMockEmbeddedServer.applicationContext.containsBean(OpenIdConfigurationController)
-        assert mockOpenIdHttpServerUrl != null
-        "${mockOpenIdHttpServerUrl}${mockServerPath}"
-    }
-
-    Map<String, Object> getConfiguration() {
-        [
-                'micronaut.security.token.jwt.bearer.enabled': false,
-                'micronaut.security.token.jwt.cookie.enabled': true,
-                'micronaut.security.oauth2.clients.foo.client-id': 'XXXX',
-                'micronaut.security.oauth2.clients.foo.client-secret': 'YYYY',
-                'micronaut.security.oauth2.clients.foo.openid.issuer': getIssuer(),
-        ]
-    }
-
-    void setup() {
-        mockOpenIdHttpServerPort = SocketUtils.findAvailableTcpPort()
-        mockOpenIdHttpServerUrl = "http://localhost:${mockOpenIdHttpServerPort}"
-        openIdMockServerConf = [
-                "spec.name": "mockopenidprovider",
-                'micronaut.server.port': mockOpenIdHttpServerPort,
-                'mockserver.url': mockOpenIdHttpServerUrl,
-                'mockserver.path': mockServerPath,
-        ] as Map<String, Object>
-
-        openIdMockEmbeddedServer = ApplicationContext.run(EmbeddedServer, openIdMockServerConf)
-        applicationContext = ApplicationContext.run(configuration)
-    }
-
-    void cleanup() {
-        applicationContext.close()
-        openIdMockEmbeddedServer.close()
-    }
+    @Shared
+    @AutoCleanup
+    ApplicationContext applicationContext = ApplicationContext.run(configuration)
 }
---
+++ security-oauth2/src/test/groovy/io/micronaut/security/oauth2/ConfigurationFixture.groovy
@@ -0,0 +1,45 @@
+package io.micronaut.security.oauth2
+
+trait ConfigurationFixture {
+    Map<String, Object> getConfiguration() {
+        Map<String, Object> m = [:]
+        if (specName) {
+            m['spec.name'] = specName
+        }
+        m += disableBearerConfiguration
+        m += enableCookieConfiguration
+        m += oauth2ClientConfiguration
+        m
+    }
+
+    String getOpenIdClientName() {
+        'foo'
+    }
+
+    String getSpecName() {
+        null
+    }
+
+    String getIssuer() {
+        null
+    }
+
+    Map<String, Object> getDisableBearerConfiguration() {
+        ['micronaut.security.token.jwt.bearer.enabled': false]
+    }
+
+    Map<String, Object> getEnableCookieConfiguration() {
+        ['micronaut.security.token.jwt.cookie.enabled': true] as Map<String, Object>
+    }
+
+    Map<String, Object> getOauth2ClientConfiguration() {
+        Map m = [
+                ("micronaut.security.oauth2.clients.${openIdClientName}.client-id".toString()): 'XXXX',
+                ("micronaut.security.oauth2.clients.${openIdClientName}.client-secret".toString()): 'YYYY',
+        ]
+        if (issuer != null) {
+            m[("micronaut.security.oauth2.clients.${openIdClientName}.openid.issuer".toString())] = issuer
+        }
+        m
+    }
+}
