fe5e9d71e8c5168fbd11115d91c896a60c073e7f
Sergio del Amo <sergio.delamo@softamo.com>
2020-05-15 16:48:37 +0200

    test: rewrite OpenIdClientConditionSpec

--- security-oauth2/src/test/groovy/io/micronaut/security/oauth2/client/condition/OpenIdClientConditionSpec.groovy
+++ security-oauth2/src/test/groovy/io/micronaut/security/oauth2/client/condition/OpenIdClientConditionSpec.groovy
@@ -2,29 +2,23 @@ package io.micronaut.security.oauth2.client.condition
 
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.env.Environment
+import io.micronaut.security.oauth2.ApplicationContextSpecification
 import io.micronaut.security.oauth2.client.DefaultOpenIdClient
-import spock.lang.Specification
 
-class OpenIdClientConditionSpec extends Specification {
-    static final SPEC_NAME_PROPERTY = 'spec.name'
+class OpenIdClientConditionSpec extends ApplicationContextSpecification {
 
-    void "OpenIdClientCondition evaluates to true for openid client with manual token and authorization urls"() {
-        given:
-        ApplicationContext context = ApplicationContext.run([
-                (SPEC_NAME_PROPERTY): getClass().simpleName,
+    @Override
+    Map<String, Object> getConfiguration() {
+        super.configuration + [
                 'micronaut.security.oauth2.clients.foo.client-id': 'XXXX',
                 'micronaut.security.oauth2.clients.foo.client-secret': 'YYYY',
                 'micronaut.security.oauth2.clients.foo.openid.authorization.url': 'https://appleid.apple.com/auth/authorize',
                 'micronaut.security.oauth2.clients.foo.openid.token.url': 'https://appleid.apple.com/auth/token'
-        ], Environment.TEST)
+        ]
+    }
 
-        when:
-        context.getBean(DefaultOpenIdClient)
-
-        then:
-        noExceptionThrown()
-
-        cleanup:
-        context.close()
+    void "OpenIdClientCondition evaluates to true for openid client with manual token and authorization urls"() {
+        expect:
+        applicationContext.containsBean(DefaultOpenIdClient)
     }
 }
