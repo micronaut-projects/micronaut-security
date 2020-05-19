64874ba91f15f6ed3828c24db1caa0588651dd57
Sergio del Amo <sergio.delamo@softamo.com>
2020-05-19 10:20:18 +0200

    BREAKING CHANGE: JwtGeneratorConfig & deprecate it

---
+++ security-jwt/src/main/java/io/micronaut/security/token/jwt/generator/JwtGeneratorConfigurationProperties.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017-2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.micronaut.security.token.jwt.generator;
+
+import javax.inject.Singleton;
+
+/**
+ * {@link JwtGeneratorConfiguration} implementation.
+ *
+ * @author Sergio del Amo
+ * @since 1.0
+ * @deprecated Use {@link AccessTokenConfigurationProperties} instead.
+ */
+@Deprecated
+@Singleton
+public class JwtGeneratorConfigurationProperties implements JwtGeneratorConfiguration {
+
+    private final AccessTokenConfiguration accessTokenConfiguration;
+
+    /**
+     *
+     * @param accessTokenConfiguration Access TokenConfiguration
+     */
+    public JwtGeneratorConfigurationProperties(AccessTokenConfiguration accessTokenConfiguration) {
+        this.accessTokenConfiguration = accessTokenConfiguration;
+    }
+
+    @Override
+    @Deprecated
+    public Integer getRefreshTokenExpiration() {
+        return null;
+    }
+
+    /**
+     * @deprecated Use {@link AccessTokenConfiguration#getExpiration()}) instead.
+     */
+    @Override
+    @Deprecated
+    public Integer getAccessTokenExpiration() {
+        return accessTokenConfiguration.getExpiration();
+    }
+}
