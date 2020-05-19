188ca28d3a7dd2a5cabc1b21543bfbdcb860bbe7
Sergio del Amo <sergio.delamo@softamo.com>
2020-05-19 11:54:58 +0200

    build: consolidate GebConfig

--- security-session/src/test/resources/GebConfig.groovy
+++ security-session/src/test/resources/GebConfig.groovy
@@ -1,16 +1,19 @@
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxOptions
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
+
+driver = {
+    new HtmlUnitDriver(true)
+}
 
 environments {
 
-    // run via “./gradlew -Dgeb.env=chrome iT”
     chrome {
         driver = { new ChromeDriver() }
     }
 
-    // run via “./gradlew -Dgeb.env=chromeHeadless iT”
     chromeHeadless {
         driver = {
             ChromeOptions o = new ChromeOptions()
@@ -19,11 +22,17 @@ environments {
         }
     }
 
-    // run via “./gradlew -Dgeb.env=htmlunit iT”
-    htmlunit { driver = { new HtmlUnitDriver(true) } }
+    firefoxHeadless {
+        driver = {
+            FirefoxOptions o = new FirefoxOptions()
+            o.addArguments('-headless')
+            new FirefoxDriver(o)
+        }
+    }
 
-    // run via “./gradlew -Dgeb.env=firefox iT”
     firefox {
         driver = { new FirefoxDriver() }
     }
+
+    htmlunit { driver = { new HtmlUnitDriver(true) } }
 }
