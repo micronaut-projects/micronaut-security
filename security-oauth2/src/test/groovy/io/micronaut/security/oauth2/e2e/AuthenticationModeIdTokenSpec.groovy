c4a07949c58e241957799e0ab578ddcb996b39c9
Sergio del Amo <sergio.delamo@softamo.com>
2020-06-05 16:06:23 +0200

    test: remove test leakage logout from keycloak

--- security-oauth2/src/test/groovy/io/micronaut/security/oauth2/e2e/AuthenticationModeIdTokenSpec.groovy
+++ security-oauth2/src/test/groovy/io/micronaut/security/oauth2/e2e/AuthenticationModeIdTokenSpec.groovy
@@ -1,17 +1,27 @@
 package io.micronaut.security.oauth2.e2e
 
+import edu.umd.cs.findbugs.annotations.Nullable
 import io.micronaut.context.annotation.Requires
+import io.micronaut.http.HttpRequest
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.server.util.HttpHostResolver
+import io.micronaut.http.uri.UriBuilder
 import io.micronaut.security.annotation.Secured
+import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.oauth2.GebEmbeddedServerSpecification
 import io.micronaut.security.oauth2.Keycloak
+import io.micronaut.security.oauth2.client.OpenIdProviderMetadata
+import io.micronaut.security.oauth2.configuration.endpoints.EndSessionConfiguration
+import io.micronaut.security.oauth2.endpoint.endsession.request.EndSessionEndpoint
 import io.micronaut.security.rules.SecurityRule
 import io.micronaut.security.token.jwt.signature.jwks.JwksSignature
 import io.micronaut.security.token.validator.TokenValidator
 import spock.lang.IgnoreIf
 
+import javax.inject.Named
+import javax.inject.Singleton
 import java.security.Principal
 
 @IgnoreIf({ sys['testcontainers'] == false })
@@ -29,6 +39,7 @@ class AuthenticationModeIdTokenSpec extends GebEmbeddedServerSpecification {
                 "micronaut.security.oauth2.clients.keycloak.openid.issuer" : Keycloak.issuer,
                 "micronaut.security.oauth2.clients.keycloak.client-id" : Keycloak.CLIENT_ID,
                 "micronaut.security.oauth2.clients.keycloak.client-secret" : Keycloak.clientSecret,
+                "micronaut.security.endpoints.logout.get-allowed": true,
         ] as Map<String, Object>
     }
 
@@ -57,17 +68,59 @@ class AuthenticationModeIdTokenSpec extends GebEmbeddedServerSpecification {
         HomePage homePage = browser.page HomePage
 
         then:
+        !homePage.message.contains("Hello anonymous")
         homePage.message.matches("Hello .*")
+
+        when:
+        via OAuthLogoutPage
+
+        then:
+        at HomePage
+
+        when:
+        homePage = browser.page HomePage
+
+        then:
+        homePage.message.contains("Hello anonymous")
     }
 
     @Requires(property = 'spec.name', value = 'AuthenticationModeIdTokenSpec')
-    @Secured(SecurityRule.IS_AUTHENTICATED)
+    @Secured(SecurityRule.IS_ANONYMOUS)
     @Controller
     static class HomeController {
 
         @Get(produces = MediaType.TEXT_HTML)
-        String index(Principal principal) {
-            "<html><head><title>Home</title></head><body>Hello ${principal.name}</body></html>"
+        String index(@Nullable Principal principal) {
+            "<html><head><title>Home</title></head><body>Hello ${principal ? principal.name : 'anonymous'}</body></html>"
+        }
+    }
+
+    @Singleton
+    @Named("keycloak")
+    @Requires(property = 'spec.name', value = 'AuthenticationModeIdTokenSpec')
+    static class KeycloakEndSessionEndpoint implements EndSessionEndpoint {
+
+        public static final String PARAM_REDIRECT_URI = "redirect_uri"
+        private final OpenIdProviderMetadata openIdProviderMetadata
+        private final EndSessionConfiguration endSessionConfiguration
+        private final HttpHostResolver httpHostResolver
+
+        KeycloakEndSessionEndpoint(@Named("keycloak") OpenIdProviderMetadata openIdProviderMetadata,
+                                   EndSessionConfiguration endSessionConfiguration,
+                                   HttpHostResolver httpHostResolver) {
+            this.openIdProviderMetadata = openIdProviderMetadata
+            this.endSessionConfiguration = endSessionConfiguration
+            this.httpHostResolver = httpHostResolver
+        }
+
+        @Nullable
+        @Override
+        String getUrl(HttpRequest originating, Authentication authentication) {
+            (openIdProviderMetadata.getEndSessionEndpoint() == null) ? null :
+                    UriBuilder.of(URI.create(openIdProviderMetadata.getEndSessionEndpoint()))
+                            .queryParam(PARAM_REDIRECT_URI, httpHostResolver.resolve(originating) + endSessionConfiguration.getRedirectUri())
+                            .build()
+                            .toString()
         }
     }
 }
--- security-oauth2/src/test/groovy/io/micronaut/security/oauth2/e2e/OpenIdAuthorizationCodeSpec.groovy
+++ security-oauth2/src/test/groovy/io/micronaut/security/oauth2/e2e/OpenIdAuthorizationCodeSpec.groovy
@@ -1,15 +1,25 @@
 package io.micronaut.security.oauth2.e2e
 
+import edu.umd.cs.findbugs.annotations.Nullable
 import io.micronaut.context.annotation.Requires
+import io.micronaut.http.HttpRequest
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.server.util.HttpHostResolver
+import io.micronaut.http.uri.UriBuilder
 import io.micronaut.security.annotation.Secured
+import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.oauth2.GebEmbeddedServerSpecification
 import io.micronaut.security.oauth2.Keycloak
+import io.micronaut.security.oauth2.client.OpenIdProviderMetadata
+import io.micronaut.security.oauth2.configuration.endpoints.EndSessionConfiguration
+import io.micronaut.security.oauth2.endpoint.endsession.request.EndSessionEndpoint
 import io.micronaut.security.rules.SecurityRule
 import spock.lang.IgnoreIf
 
+import javax.inject.Named
+import javax.inject.Singleton
 import java.security.Principal
 
 @IgnoreIf({ sys['testcontainers'] == false })
@@ -28,6 +38,7 @@ class OpenIdAuthorizationCodeSpec extends GebEmbeddedServerSpecification {
                 "micronaut.security.oauth2.clients.keycloak.client-id" : Keycloak.CLIENT_ID,
                 "micronaut.security.oauth2.clients.keycloak.client-secret" : Keycloak.clientSecret,
                 "micronaut.security.token.jwt.signatures.secret.generator.secret" : 'pleaseChangeThisSecretForANewOne',
+                "micronaut.security.endpoints.logout.get-allowed": true,
         ] as Map<String, Object>
     }
 
@@ -52,17 +63,59 @@ class OpenIdAuthorizationCodeSpec extends GebEmbeddedServerSpecification {
         HomePage homePage = browser.page HomePage
 
         then:
+        !homePage.message.contains("Hello anonymous")
         homePage.message.matches("Hello .*")
+
+        when:
+        via OAuthLogoutPage
+
+        then:
+        at HomePage
+
+        when:
+        homePage = browser.page HomePage
+
+        then:
+        homePage.message.contains("Hello anonymous")
     }
 
     @Requires(property = 'spec.name', value = 'OpenIdAuthorizationCodeSpec')
-    @Secured(SecurityRule.IS_AUTHENTICATED)
+    @Secured(SecurityRule.IS_ANONYMOUS)
     @Controller
     static class HomeController {
 
         @Get(produces = MediaType.TEXT_HTML)
-        String index(Principal principal) {
-            "<html><head><title>Home</title></head><body>Hello ${principal.name}</body></html>"
+        String index(@Nullable Principal principal) {
+            "<html><head><title>Home</title></head><body>Hello ${principal ? principal.name : 'anonymous'}</body></html>"
+        }
+    }
+
+    @Singleton
+    @Named("keycloak")
+    @Requires(property = 'spec.name', value = 'OpenIdAuthorizationCodeSpec')
+    static class KeycloakEndSessionEndpoint implements EndSessionEndpoint {
+
+        public static final String PARAM_REDIRECT_URI = "redirect_uri"
+        private final OpenIdProviderMetadata openIdProviderMetadata
+        private final EndSessionConfiguration endSessionConfiguration
+        private final HttpHostResolver httpHostResolver
+
+        KeycloakEndSessionEndpoint(@Named("keycloak") OpenIdProviderMetadata openIdProviderMetadata,
+                                          EndSessionConfiguration endSessionConfiguration,
+                                          HttpHostResolver httpHostResolver) {
+            this.openIdProviderMetadata = openIdProviderMetadata
+            this.endSessionConfiguration = endSessionConfiguration
+            this.httpHostResolver = httpHostResolver
+        }
+
+        @Nullable
+        @Override
+        String getUrl(HttpRequest originating, Authentication authentication) {
+            (openIdProviderMetadata.getEndSessionEndpoint() == null) ? null :
+                    UriBuilder.of(URI.create(openIdProviderMetadata.getEndSessionEndpoint()))
+                            .queryParam(PARAM_REDIRECT_URI, httpHostResolver.resolve(originating) + endSessionConfiguration.getRedirectUri())
+                            .build()
+                            .toString()
         }
     }
 }
