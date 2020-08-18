8aa0a05ccaa93f616b6818b708f9fad3a095f674
Sergio del Amo <sergio.delamo@softamo.com>
2020-08-18 07:59:43 +0200

    feat: allow NonceClaimValidator to be disabled

---
+++ security-oauth2/src/test/groovy/io/micronaut/security/oauth2/endpoint/token/response/validation/NonceClaimValidatorDisableSpec.groovy
@@ -0,0 +1,16 @@
+package io.micronaut.security.oauth2.endpoint.token.response.validation
+
+import io.micronaut.security.oauth2.ApplicationContextSpecification
+
+class NonceClaimValidatorDisableSpec extends ApplicationContextSpecification {
+
+    @Override
+    Map<String, Object> getConfiguration() {
+        super.configuration + ['micronaut.security.token.jwt.claims-validators.nonce': false]
+    }
+
+    void "NonceClaimsValidator can be disabled with micronaut.security.token.jwt.claims-validators.nonce.enabled"() {
+        expect:
+        !applicationContext.containsBean(NonceClaimValidator)
+    }
+}
