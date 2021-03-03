78e64eb4f3fa201a586f08464f73952bd1cd4ab2
Steven Brown <steven.brown@keap.com>
2021-03-02 19:42:30 -0700

    Update tests and add a small blurb to docs

--- security/src/test/groovy/io/micronaut/security/filters/SecurityFilterSpec.groovy
+++ security/src/test/groovy/io/micronaut/security/filters/SecurityFilterSpec.groovy
@@ -1,121 +1,177 @@
 package io.micronaut.security.filters
 
-import io.micronaut.core.convert.value.MutableConvertibleValues
-import io.micronaut.http.HttpMethod
+import io.micronaut.context.annotation.Property
+import io.micronaut.context.annotation.Requires
+import io.micronaut.core.util.StringUtils
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.filter.ServerFilterChain
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.security.EmbeddedServerSpecification
-import io.micronaut.security.authentication.Authentication
-import io.micronaut.security.authentication.AuthorizationException
-import io.micronaut.security.config.SecurityConfiguration
+import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.ReactiveSecurityRule
 import io.micronaut.security.rules.SecurityRule
 import io.micronaut.security.rules.SecurityRuleResult
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import io.micronaut.web.router.RouteMatch
 import io.reactivex.Flowable
 import io.reactivex.schedulers.Schedulers
-import io.reactivex.subscribers.TestSubscriber
-import spock.lang.Unroll
+import org.jetbrains.annotations.Nullable
+import org.reactivestreams.Publisher
 
+import javax.inject.Singleton
 import java.util.concurrent.TimeUnit
 
 import static io.micronaut.security.rules.SecurityRuleResult.ALLOWED
 import static io.micronaut.security.rules.SecurityRuleResult.REJECTED
 import static io.micronaut.security.rules.SecurityRuleResult.UNKNOWN
 
+@MicronautTest(rebuildContext = true)
 class SecurityFilterSpec extends EmbeddedServerSpecification {
+
+    static final String magicString = 'Hello world'
 
     @Override
     String getSpecName() {
         'SecurityFilterSpec'
     }
 
-    @Unroll('#description')
-    def "Filter order is correct"() {
+    @Property(name = "disable.all.rules", value = StringUtils.TRUE)
+    def 'No rules'() {
         given:
-        SecurityFilter filter = new SecurityFilter(rules, reactiveRules, List.of(), Stub(SecurityConfiguration.class))
+        HttpRequest request = HttpRequest.GET('/securityFilter')
 
-        HttpRequest request = Stub(HttpRequest.class) {
-            getPath() >> new URI('/')
-            getMethod() >> HttpMethod.GET
-            getAttributes() >> Stub(MutableConvertibleValues.class)
-        }
+        when:
+        client.exchange(request, String)
 
-        ServerFilterChain chain = Stub(ServerFilterChain.class) {
-            proceed(request) >> {allowed != null ? Flowable.just(HttpResponse.ok()) : Flowable.empty()}
-        }
+        then:
+        HttpClientResponseException e = thrown(HttpClientResponseException)
+        e.status == HttpStatus.UNAUTHORIZED
+    }
 
-        RouteMatch<?> routeMatch = Stub(RouteMatch.class)
-        Authentication authentication = Stub(Authentication.class)
+    @Property(name = "disable.all.rules", value = StringUtils.FALSE)
+    def 'With rules - authorized'() {
+        given:
+        HttpRequest request = HttpRequest.GET('/securityFilter')
 
-        TestSubscriber<HttpResponse<?>> testSubscriber = new TestSubscriber<>()
+        when:
+        Rule1.result = ruleValues[0]
+        Rule2.result = ruleValues[1]
+        Rule3.result = ruleValues[2]
 
-        expect:
-        filter.checkRules(request, chain, routeMatch, authentication).subscribe(testSubscriber)
-        testSubscriber.await()
+        Rule1.delayMs = delayValue
 
-        if (allowed) {
-            testSubscriber.completions() == 1
-            !testSubscriber.timeout
-            testSubscriber.errorCount() == 0
-            testSubscriber.valueCount() == 1
-            testSubscriber.values() == [HttpResponse.ok()]
-        } else {
-            testSubscriber.completions() == 0
-            !testSubscriber.timeout
-            testSubscriber.errorCount() == 1
-            testSubscriber.errors().get(0).class == AuthorizationException.class
-            testSubscriber.valueCount() == 0
-        }
+        HttpResponse<String> response = client.exchange(request, String)
+
+        then:
+        response != null
+        response.getStatus() == HttpStatus.OK
+        response.body() != null
+        response.body() == magicString
 
         where:
-        rules                       | reactiveRules                           | allowed | description
-        []                          | []                                      | false   | 'No rules'
-        [securityRule(0, ALLOWED)]  | []                                      | true    | 'One SecurityRule - allowed'
-        [securityRule(0, REJECTED)] | []                                      | false   | 'One SecurityRule - rejected'
-        [securityRule(0, UNKNOWN)]  | []                                      | false   | 'One SecurityRule - unknown'
-        []                          | [reactiveSecurityRule(0, ALLOWED, 0)]   | true    | 'One ReactiveSecurityRule - allowed'
-        []                          | [reactiveSecurityRule(0, REJECTED, 0)]  | false   | 'One ReactiveSecurityRule - rejected'
-        []                          | [reactiveSecurityRule(0, UNKNOWN, 0)]   | false   | 'One ReactiveSecurityRule - unknown'
-
-        // Non-reactive first
-        [securityRule(0, UNKNOWN)]  | [reactiveSecurityRule(1, ALLOWED, 0)]   | true    | 'Mixed - rule unknown => reactive allowed, no delay'
-        [securityRule(0, ALLOWED)]  | [reactiveSecurityRule(1, REJECTED, 0)]  | true    | 'Mixed - rule allowed => reactive rejected, no delay'
-        [securityRule(0, UNKNOWN)]  | [reactiveSecurityRule(1, REJECTED, 0)]  | false   | 'Mixed - rule unknown => reactive rejected, no delay'
-        [securityRule(0, REJECTED)] | [reactiveSecurityRule(1, ALLOWED, 0)]   | false   | 'Mixed - rule rejected => reactive allowed, no delay'
-        [securityRule(0, UNKNOWN)]  | [reactiveSecurityRule(1, UNKNOWN, 0)]   | false   | 'Mixed - rule unknown => reactive unknown, no delay'
-
-        // Reactive first
-        [securityRule(1, ALLOWED)]  | [reactiveSecurityRule(0, UNKNOWN, 0)]   | true    | 'Mixed - reactive unknown => rule allowed, no delay'
-        [securityRule(1, REJECTED)] | [reactiveSecurityRule(0, ALLOWED, 0)]   | true    | 'Mixed - reactive allowed => rule rejected, no delay'
-        [securityRule(1, REJECTED)] | [reactiveSecurityRule(0, UNKNOWN, 0)]   | false   | 'Mixed - reactive unknown => rule rejected, no delay'
-        [securityRule(1, ALLOWED)]  | [reactiveSecurityRule(0, REJECTED, 0)]  | false   | 'Mixed - reactive rejected => rule allowed, no delay'
-        [securityRule(1, UNKNOWN)]  | [reactiveSecurityRule(0, UNKNOWN, 0)]   | false   | 'Mixed - reactive unknown => rule unknown, no delay'
+        ruleValues                    | delayValue | description
+        [ALLOWED, REJECTED, REJECTED] | 0          | 'first rule - allowed'
+        [UNKNOWN, ALLOWED, REJECTED]  | 0          | 'second rule - allowed'
+        [UNKNOWN, UNKNOWN, ALLOWED]   | 0          | 'third rule - allowed'
 
         // With delays (cause the reactive to evaluate async and slowly, ensure still correct response)
-        [securityRule(1, REJECTED)] | [reactiveSecurityRule(0, ALLOWED, 100)]  | true   | 'Mixed - reactive allowed => rule rejected, 100ms delay'
-        [securityRule(1, ALLOWED)]  | [reactiveSecurityRule(0, REJECTED, 100)] | false  | 'Mixed - reactive rejected => rule allowed, 100ms delay'
+        [ALLOWED, REJECTED, REJECTED] | 100        | 'first allowed after 100ms delay'
 
-        // Multiple of same type, injection sort order incorrect
-        [securityRule(1, REJECTED), securityRule(0, ALLOWED)]  | [] | true  | 'Multiple - rule 0 rejected => rule 1 allowed, no delay'
-        [] | [reactiveSecurityRule(1, REJECTED, 0), reactiveSecurityRule(0, ALLOWED, 0)] | true  | 'Multiple - reactive 0 rejected => reactive 1 allowed, no delay'
     }
 
-    SecurityRule securityRule(int order, SecurityRuleResult result) {
-        Stub(SecurityRule.class) {
-            check(_ as HttpRequest, _ as RouteMatch, _ as Map) >> result
-            getOrder() >> order
+    @Property(name = "disable.all.rules", value = StringUtils.FALSE)
+    def 'With rules - unauthorized'() {
+        given:
+        HttpRequest request = HttpRequest.GET('/securityFilter')
+
+        when:
+        Rule1.result = ruleValues[0]
+        Rule2.result = ruleValues[1]
+        Rule3.result = ruleValues[2]
+
+        Rule1.delayMs = delayValue
+
+        client.exchange(request, String)
+
+        then:
+        HttpClientResponseException e = thrown(HttpClientResponseException)
+        e.status == HttpStatus.UNAUTHORIZED
+
+        where:
+        ruleValues                            | delayValue | description
+        [UNKNOWN, UNKNOWN, UNKNOWN]  | 0      | 'all unknown'
+        [REJECTED, ALLOWED, UNKNOWN] | 0      | 'first rule - rejected'
+        [UNKNOWN, REJECTED, ALLOWED] | 0      | 'second rule - rejected'
+        [UNKNOWN, UNKNOWN, REJECTED] | 0      | 'third rule - rejected'
+
+        // With delays (cause the reactive to evaluate async and slowly, ensure still correct response)
+        [REJECTED, ALLOWED, ALLOWED] | 100    | 'first rejected after 100ms delay'
+    }
+
+    @Singleton
+    @Requires(property = "spec.name", value = 'SecurityFilterSpec')
+    @Requires(property = "disable.all.rules", notEquals = StringUtils.TRUE)
+    static class Rule1 implements ReactiveSecurityRule {
+        static SecurityRuleResult result = UNKNOWN
+        static int delayMs = 0
+
+        @Override
+        Publisher<SecurityRuleResult> check(HttpRequest<?> request, @Nullable RouteMatch<?> routeMatch, @Nullable Map<String, Object> claims) {
+            return Flowable.timer(delayMs, TimeUnit.MILLISECONDS)
+                    .subscribeOn(Schedulers.io())
+                    .flatMap((l) -> Flowable.just(result))
+        }
+
+        @Override
+        int getOrder() {
+            return HIGHEST_PRECEDENCE
         }
     }
 
-    ReactiveSecurityRule reactiveSecurityRule(int order, SecurityRuleResult result, long delayMs) {
-        Stub(ReactiveSecurityRule.class) {
-            check(_ as HttpRequest, _ as RouteMatch, _ as Map) >>
-                    Flowable.timer(delayMs, TimeUnit.MILLISECONDS)
-                            .subscribeOn(Schedulers.io())
-                            .flatMap((l) -> Flowable.just(result))
-            getOrder() >> order
+    @Singleton
+    @Requires(property = "spec.name", value = 'SecurityFilterSpec')
+    @Requires(property = "disable.all.rules", notEquals = StringUtils.TRUE)
+    static class Rule2 implements SecurityRule {
+        static SecurityRuleResult result = UNKNOWN
+
+        @Override
+        SecurityRuleResult check(HttpRequest<?> request, @Nullable RouteMatch<?> routeMatch, @Nullable Map<String, Object> claims) {
+            return result
+        }
+
+        @Override
+        int getOrder() {
+            return HIGHEST_PRECEDENCE + 1
+        }
+    }
+
+    @Singleton
+    @Requires(property = "spec.name", value = 'SecurityFilterSpec')
+    @Requires(property = "disable.all.rules", notEquals = StringUtils.TRUE)
+    static class Rule3 implements ReactiveSecurityRule {
+        static SecurityRuleResult result = UNKNOWN
+
+        @Override
+        Publisher<SecurityRuleResult> check(HttpRequest<?> request, @Nullable RouteMatch<?> routeMatch, @Nullable Map<String, Object> claims) {
+            return Flowable.just(result)
+        }
+
+        @Override
+        int getOrder() {
+            return HIGHEST_PRECEDENCE + 2
+        }
+    }
+
+    @Requires(property = "spec.name", value = 'SecurityFilterSpec')
+    @Controller('/securityFilter')
+    @Secured(SecurityRule.IS_AUTHENTICATED)
+    static class TestController {
+        @Get
+        String get() {
+            return magicString
         }
     }
 }
