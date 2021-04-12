14497fe4ab0d31b7e8e19e98a654e539ee6311a2
Sergio del Amo <sergio.delamo@softamo.com>
2021-04-12 19:12:21 +0200

    docs: improve documentation about SensitiveEndpointRule replacement (#619)
    * refactor: SensitiveEndpointRule constructor public
    
    * docs: improve documentation about SensitiveEndpointRule replacement
    
    close: https://github.com/micronaut-projects/micronaut-security/issues/606
    close: https://github.com/micronaut-projects/micronaut-security/pull/589

---
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/security/oauth2/docs/managementendpoints/SensitiveEndpointRuleReplacement.kt
@@ -0,0 +1,30 @@
+package io.micronaut.security.oauth2.docs.managementendpoints
+
+//tag::imports[]
+import io.micronaut.context.annotation.Replaces
+import io.micronaut.context.annotation.Requires
+import io.micronaut.core.annotation.NonNull
+import io.micronaut.http.HttpRequest
+import io.micronaut.inject.ExecutableMethod
+import io.micronaut.management.endpoint.EndpointSensitivityProcessor
+import io.micronaut.security.rules.SecurityRuleResult
+import io.micronaut.security.rules.SensitiveEndpointRule
+import io.micronaut.security.token.MapClaims
+import io.micronaut.security.token.RolesFinder
+import javax.inject.Singleton
+//end::imports[]
+
+@Requires(property = "spec.name", value = "LoggersSpec")
+//tag::clazz[]
+@Replaces(SensitiveEndpointRule::class)
+@Singleton
+class SensitiveEndpointRuleReplacement(endpointSensitivityProcessor: EndpointSensitivityProcessor,
+                                       private val rolesFinder: RolesFinder) : SensitiveEndpointRule(endpointSensitivityProcessor) {
+    @NonNull
+    override fun checkSensitiveAuthenticated(@NonNull request: HttpRequest<*>,
+                                             @NonNull claims: Map<String, Any>,
+                                             @NonNull method: ExecutableMethod<*, *>): SecurityRuleResult {
+        return if (rolesFinder.hasAnyRequiredRoles(listOf("ROLE_SYSTEM"), MapClaims(claims))) SecurityRuleResult.ALLOWED else SecurityRuleResult.REJECTED
+    }
+}
+//end::clazz[]
---
+++ test-suite/src/test/java/io/micronaut/security/oauth2/docs/managementendpoints/SensitiveEndpointRuleReplacement.java
@@ -0,0 +1,41 @@
+package io.micronaut.security.oauth2.docs.managementendpoints;
+
+//tag::imports[]
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.context.annotation.Replaces;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.inject.ExecutableMethod;
+import io.micronaut.management.endpoint.EndpointSensitivityProcessor;
+import io.micronaut.security.rules.SecurityRuleResult;
+import io.micronaut.security.rules.SensitiveEndpointRule;
+import io.micronaut.security.token.MapClaims;
+import io.micronaut.security.token.RolesFinder;
+import javax.inject.Singleton;
+import java.util.Collections;
+import java.util.Map;
+//end::imports[]
+
+@Requires(property = "spec.name", value = "LoggersSpec")
+//tag::clazz[]
+@Replaces(SensitiveEndpointRule.class)
+@Singleton
+public class SensitiveEndpointRuleReplacement extends SensitiveEndpointRule {
+    private final RolesFinder rolesFinder;
+
+    public SensitiveEndpointRuleReplacement(EndpointSensitivityProcessor endpointSensitivityProcessor,
+                                            RolesFinder rolesFinder) {
+        super(endpointSensitivityProcessor);
+        this.rolesFinder = rolesFinder;
+    }
+
+    @Override
+    @NonNull
+    protected SecurityRuleResult checkSensitiveAuthenticated(@NonNull HttpRequest<?> request,
+                                                             @NonNull Map<String, Object> claims,
+                                                             @NonNull ExecutableMethod<?, ?> method) {
+        return rolesFinder.hasAnyRequiredRoles(Collections.singletonList("ROLE_SYSTEM"), new MapClaims(claims))
+                    ? SecurityRuleResult.ALLOWED : SecurityRuleResult.REJECTED;
+    }
+}
+//end::clazz[]
