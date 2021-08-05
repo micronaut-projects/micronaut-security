cd456da126760e890de34ddf40580ec0296469cb
Sergio del Amo <sergio.delamo@softamo.com>
2021-08-05 13:46:36 +0200

    test: use Authentication for SecurityService case

--- security/src/test/groovy/io/micronaut/security/authorization/SecuredRolesCaseSensitiveSpec.groovy
+++ security/src/test/groovy/io/micronaut/security/authorization/SecuredRolesCaseSensitiveSpec.groovy
@@ -12,7 +12,6 @@ import io.micronaut.security.MockAuthenticationProvider
 import io.micronaut.security.SuccessAuthenticationScenario
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.authentication.Authentication
-import io.micronaut.security.authentication.ClientAuthentication
 import io.micronaut.security.testutils.EmbeddedServerSpecification
 import io.micronaut.security.token.RolesFinder
 import io.micronaut.security.utils.DefaultSecurityService
@@ -45,18 +44,8 @@ class SecuredRolesCaseSensitiveSpec extends EmbeddedServerSpecification {
 
     void "SecurityService::hasRole should be case sensitive"() {
         when:
-        Authentication authentication = new ClientAuthentication("sherlock", ["roles": ["ROLE_DETECTIVE"], "rolesKey": "roles"])
+        Authentication authentication = Authentication.build("sherlock", ["ROLE_DETECTIVE"])
         SecurityService securityService = new CustomSecurityService(applicationContext.getBean(RolesFinder), authentication)
-
-        then:
-        securityService.hasRole('ROLE_DETECTIVE')
-
-        and:
-        !securityService.hasRole('role_detective')
-
-        when:
-        authentication = new ClientAuthentication("sherlock", ["roles": "ROLE_DETECTIVE", "rolesKey": "roles"])
-        securityService = new CustomSecurityService(applicationContext.getBean(RolesFinder), authentication)
 
         then:
         securityService.hasRole('ROLE_DETECTIVE')
