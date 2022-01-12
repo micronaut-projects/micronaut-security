f743e2617a5b69214938335cad6d296c7030b827
sdelamo <sergio.delamo@softamo.com>
2022-01-12 06:46:54 +0100

    test: rewrite test using EmbeddedServerSpecification

--- security/src/test/groovy/io/micronaut/docs/security/authorization/SiteminderAuthorizationSpec.groovy
+++ security/src/test/groovy/io/micronaut/docs/security/authorization/SiteminderAuthorizationSpec.groovy
@@ -1,35 +1,33 @@
 package io.micronaut.docs.security.authorization
 
-import io.micronaut.context.ApplicationContext
 import io.micronaut.context.annotation.Requires
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
-import io.micronaut.http.client.HttpClient
-import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.rules.SecurityRule
-import spock.lang.Specification
+import io.micronaut.security.testutils.EmbeddedServerSpecification
 
 import static io.micronaut.docs.security.authorization.SiteminderAuthenticationFetcher.SITEMINDER_USER_HEADER
 
-class SiteminderAuthorizationSpec extends Specification {
+class SiteminderAuthorizationSpec extends EmbeddedServerSpecification {
+    @Override
+    String getSpecName() {
+        'SiteminderAuthorizationSpec'
+    }
 
     void 'test custom SiteMinder authentication fetcher'() {
         given:
         String username = UUID.randomUUID()
-        EmbeddedServer server = ApplicationContext.run(EmbeddedServer, [
-                'spec.name': SiteminderAuthorizationSpec.simpleName])
 
         when:
-        HttpClient client = server.applicationContext.createBean(HttpClient, server.URL)
         HttpRequest request = HttpRequest.GET('/sm').header(SITEMINDER_USER_HEADER, username)
 
         then:
-        client.toBlocking().retrieve(request) == username
+        client.retrieve(request) == username
     }
 
     @Requires(property = 'spec.name', value = 'SiteminderAuthorizationSpec')
