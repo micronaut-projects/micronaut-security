75dd1682d0391bed2d9e1e400143f6d678f13785
Sergio del Amo <sergio.delamo@softamo.com>
2022-08-18 11:39:29 +0200

    test: body of the request

--- test-suite-tomcat/src/test/java/io/micronaut/security/test/tomcat/JwtLoginTest.java
+++ test-suite-tomcat/src/test/java/io/micronaut/security/test/tomcat/JwtLoginTest.java
@@ -4,6 +4,7 @@ import io.micronaut.context.annotation.Property;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.util.CollectionUtils;
 import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.client.BlockingHttpClient;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
@@ -18,7 +19,10 @@ import jakarta.inject.Singleton;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Property(name = "micronaut.security.token.jwt.signatures.secret.generator.secret", value = "pleaseChangeThisSecretForANewOne")
 @Property(name = "micronaut.security.authentication", value = "bearer")
@@ -33,8 +37,15 @@ class JwtLoginTest {
     @Test
     void loginIsPossibleInTomcatRuntime() {
         BlockingHttpClient client = httpClient.toBlocking();
-        assertDoesNotThrow(() -> client.exchange(HttpRequest.POST("/login",
-            CollectionUtils.mapOf("username", "john", "password", "bogus"))));
+        assertDoesNotThrow(() -> client.exchange(loginRequest()));
+        HttpResponse<Map> response = client.exchange(loginRequest(), Map.class);
+        assertTrue(response.getBody().isPresent());
+        assertTrue(response.getBody().get().containsKey("access_token"));
+    }
+
+    private static HttpRequest<?> loginRequest() {
+        return HttpRequest.POST("/login",
+            CollectionUtils.mapOf("username", "john", "password", "bogus"));
     }
 
     @Requires(property = "spec.name", value = "JwtLoginTest")
