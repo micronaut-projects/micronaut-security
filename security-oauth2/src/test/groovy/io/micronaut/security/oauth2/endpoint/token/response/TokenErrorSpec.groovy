36e10f7c250b91c4dc796d1abfea1229499f464d
sdelamo <sergio.delamo@softamo.com>
2022-11-03 16:32:09 +0100

    Revert "test: PendingFeature serde"
    This reverts commit 6b713f78baf1ec20bae7da43ee16dc8fb12e46b0.

--- security-oauth2/src/test/groovy/io/micronaut/security/oauth2/endpoint/token/response/TokenErrorSpec.groovy
+++ security-oauth2/src/test/groovy/io/micronaut/security/oauth2/endpoint/token/response/TokenErrorSpec.groovy
@@ -1,12 +1,10 @@
 package io.micronaut.security.oauth2.endpoint.token.response
 
 import io.micronaut.serde.ObjectMapper
-import spock.lang.PendingFeature
 import spock.lang.Specification
 
 class TokenErrorSpec extends Specification {
 
-    @PendingFeature(reason = "https://github.com/micronaut-projects/micronaut-serialization/issues/297")
     void "TokenError should be deserializable from a string"() {
         setup:
         ObjectMapper objectMapper = ObjectMapper.getDefault()
