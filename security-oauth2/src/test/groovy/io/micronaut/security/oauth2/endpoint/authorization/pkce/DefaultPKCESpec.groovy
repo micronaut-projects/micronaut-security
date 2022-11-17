596995c3b12b633548eb827d94b5a8291410f65b
Sergio del Amo <sergio.delamo@softamo.com>
2022-11-17 13:21:13 +0100

    test: DefaultPKCE is annotated with @Introspected

--- security-oauth2/src/test/groovy/io/micronaut/security/oauth2/endpoint/authorization/pkce/DefaultPKCESpec.groovy
+++ security-oauth2/src/test/groovy/io/micronaut/security/oauth2/endpoint/authorization/pkce/DefaultPKCESpec.groovy
@@ -1,6 +1,7 @@
 package io.micronaut.security.oauth2.endpoint.authorization.pkce
 
 import io.micronaut.core.annotation.ReflectiveAccess
+import io.micronaut.core.beans.BeanIntrospection
 import spock.lang.Specification
 
 class DefaultPKCESpec extends Specification {
@@ -8,5 +9,13 @@ class DefaultPKCESpec extends Specification {
     void "DefaultPKCE is annotated with ReflectiveAccess"() {
         expect:
         DefaultPKCE.class.isAnnotationPresent(ReflectiveAccess)
+    }
+    
+    void "DefaultPKCE is annotated with @Introspected"() {
+        when:
+        BeanIntrospection.getIntrospection(DefaultPKCE)
+
+        then:
+        noExceptionThrown()
     }
 }
