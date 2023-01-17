31ea2498bdfcdf4497ea3fb2ec0542e69532dc86
Sergio del Amo <sergio.delamo@softamo.com>
2023-01-17 13:40:51 +0100

    fix: multiple possible candidates for client.DefaultOpenIdProviderMetadata
    Close: #1199

--- security-oauth2/src/main/java/io/micronaut/security/oauth2/client/DefaultOpenIdClient.java
+++ security-oauth2/src/main/java/io/micronaut/security/oauth2/client/DefaultOpenIdClient.java
@@ -116,8 +116,9 @@ public class DefaultOpenIdClient implements OpenIdClient {
 
     @Override
     public Publisher<MutableHttpResponse<?>> authorizationRedirect(HttpRequest<?> originating) {
-        AuthorizationRequest authorizationRequest = beanContext.createBean(OpenIdAuthorizationRequest.class, originating, clientConfiguration);
-        String endpoint = openIdProviderMetadata.get().getAuthorizationEndpoint();
+        OpenIdProviderMetadata openIdProviderMetadataResolved = openIdProviderMetadata.get();
+        AuthorizationRequest authorizationRequest = beanContext.createBean(OpenIdAuthorizationRequest.class, originating, clientConfiguration, openIdProviderMetadataResolved);
+        String endpoint = openIdProviderMetadataResolved.getAuthorizationEndpoint();
 
         if (LOG.isTraceEnabled()) {
             LOG.trace("Starting authorization code grant flow to provider [{}]. Redirecting to [{}]", getName(), endpoint);
