c86439e248b2c784c0867fc15fee17a27bb31d9b
Sergio del Amo <sergio.delamo@softamo.com>
2023-03-02 13:27:55 +0100

    use kotlin DSL

--- security/build.gradle.kts
+++ security/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id "io.micronaut.build.internal.security-module"
+    id("io.micronaut.build.internal.security-module")
 }
 
 dependencies {
@@ -7,22 +7,20 @@ dependencies {
     annotationProcessor(mnSerde.micronaut.serde.processor)
     api(mnValidation.micronaut.validation.processor)
     implementation(mnSerde.micronaut.serde.jackson)
-
     api(mn.micronaut.http)
     api(mn.micronaut.http.server)
     api(mnValidation.micronaut.validation)
-    api project(":security-annotations")
-
+    api(projects.securityAnnotations)
     implementation(libs.reactor.core)
     compileOnly(mn.micronaut.management)
     compileOnly(mn.jackson.databind)
     testImplementation(mnReactor.micronaut.reactor)
     testImplementation(mn.micronaut.management)
     testImplementation(mn.micronaut.http.client)
-    testAnnotationProcessor project(":security-annotations")
+    testAnnotationProcessor(projects.securityAnnotations)
     testAnnotationProcessor(mn.micronaut.inject.java)
     testImplementation(mn.micronaut.http.server.netty)
-    testImplementation project(":test-suite-utils")
+    testImplementation(projects.testSuiteUtils)
     testImplementation(mn.snakeyaml)
     testImplementation(libs.bcpkix)
 }
