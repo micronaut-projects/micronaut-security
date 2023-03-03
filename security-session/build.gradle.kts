059db6a764eb876609aa03a7e41e57112b96d7ce
Sergio del Amo <sergio.delamo@softamo.com>
2023-03-03 10:36:52 +0100

    build: update to 6.3.0 (#1240)
    * use project Accessors
    
    * build: Micronaut Build Plugins 6.3.0
    
    * build: use standardized project Names
    
    * accept api changes
    
    * Update settings.gradle

--- security-session/build.gradle.kts
+++ security-session/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     annotationProcessor(mn.micronaut.graal)
     api(mn.micronaut.http)
     api(mnSession.micronaut.session)
-    api(project(":security"))
+    api(projects.micronautSecurity)
     api(mn.micronaut.http.server)
     implementation(libs.reactor.core)
     testImplementation(platform(libs.testcontainers.bom))
@@ -14,6 +14,6 @@ dependencies {
     testImplementation(mn.micronaut.http.client)
     testImplementation(mn.micronaut.inject.groovy)
     testImplementation(mn.micronaut.http.server.netty)
-    testImplementation(project(":test-suite-utils"))
-    testImplementation(project(":test-suite-utils-security"))
+    testImplementation(projects.testSuiteUtils)
+    testImplementation(projects.testSuiteUtilsSecurity)
 }
