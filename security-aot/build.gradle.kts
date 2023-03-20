d8d6ee427ef37dc221981f7779f37b806796c83c
Sergio del Amo <sergio.delamo@softamo.com>
2023-03-20 10:37:50 +0100

    build: remove reference to core.bom

--- security-aot/build.gradle.kts
+++ security-aot/build.gradle.kts
@@ -10,10 +10,9 @@ micronautBuild {
 }
 
 dependencies {
-    compileOnly(platform(mn.micronaut.core.bom))
     implementation(projects.micronautSecurityOauth2)
     implementation(projects.micronautSecurityJwt)
-    testImplementation(platform(mn.micronaut.core.bom))
+
     testImplementation(mn.micronaut.http.server.netty)
     testImplementation(projects.micronautSecurityOauth2)
     testImplementation(mnTest.micronaut.test.spock) {
