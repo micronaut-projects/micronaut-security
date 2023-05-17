736c1f48e5d334b9be40c5820f8808be4b14309a
sdelamo <sergio.delamo@softamo.com>
2023-05-17 10:58:22 +0200

    more breaks

--- test-suite-http/src/test/java/io/micronaut/security/http/HttpOptionalTest.java
+++ test-suite-http/src/test/java/io/micronaut/security/http/HttpOptionalTest.java
@@ -4,18 +4,16 @@ import io.micronaut.context.ApplicationContext;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class HttpOptionalTest {
 
     @Test
-    void httpIsOptional() {
+    void youCanEagerlyLoadEverySingletonEvenWithoutMicronautHttp() {
         ApplicationContext ctx = assertDoesNotThrow(() ->
             ApplicationContext.builder()
                 .eagerInitSingletons(true)
                 .start()
         );
-        //assertFalse(ctx.containsBean(OauthRouteUrlBuilder.class));
         ctx.close();
     }
 }
