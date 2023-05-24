b1abc022445215a325a313fbb5a936e1ab35c906
Sergio del Amo <sergio.delamo@softamo.com>
2023-05-24 22:55:19 +0200

    update javadoc

--- security/src/main/java/io/micronaut/security/token/cookie/CookieLoginHandler.java
+++ security/src/main/java/io/micronaut/security/token/cookie/CookieLoginHandler.java
@@ -101,7 +101,7 @@ public abstract class CookieLoginHandler implements RedirectingLoginHandler<Http
     /**
      * @param authenticationFailed Object encapsulates the Login failure
      * @param request The {@link HttpRequest} being executed
-     * @return A 303 HTTP Response or 200 HTTP Response if {@link CookieLoginHandler#loginFailure} is null, for example if {@link RedirectConfiguration} is disabled.
+     * @return A 303 HTTP Response or 401 HTTP Response if {@link CookieLoginHandler#loginFailure} is null, for example if {@link RedirectConfiguration} is disabled.
      */
     @Override
     public MutableHttpResponse<?> loginFailed(AuthenticationResponse authenticationFailed, HttpRequest<?> request) {
