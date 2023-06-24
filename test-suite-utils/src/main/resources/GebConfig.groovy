26492e6889a1925c121e81042f5adac8aa970d9d
Sergio del Amo <sergio.delamo@softamo.com>
2023-06-24 11:40:10 +0200

    add types for GebConfig.groovy

--- test-suite-utils/src/main/resources/GebConfig.groovy
+++ test-suite-utils/src/main/resources/GebConfig.groovy
@@ -4,15 +4,15 @@ import org.testcontainers.utility.DockerImageName
 import spock.util.environment.OperatingSystem
 
 driver = {
-    def isM1Mac = OperatingSystem.current.macOs && System.getProperty("os.arch") == 'aarch64'
-    def rawContainer = isM1Mac ?
+    boolean isM1Mac = OperatingSystem.current.macOs && System.getProperty("os.arch") == 'aarch64'
+    BrowserWebDriverContainer rawContainer = isM1Mac ?
             new BrowserWebDriverContainer(DockerImageName
                     .parse("seleniarm/standalone-firefox")
                     .asCompatibleSubstituteFor("selenium/standalone-firefox")
             ) :
             new BrowserWebDriverContainer()
 
-    def container = rawContainer.withCapabilities(new FirefoxOptions())
+    BrowserWebDriverContainer container = rawContainer.withCapabilities(new FirefoxOptions())
 
     container.start()
     container.webDriver
