6d49d8611f8cc12e0cada74af46c494ee94ea8c0
sdelamo <sergio.delamo@softamo.com>
2023-07-28 10:11:32 +0200

    depend on http.client.core
    micronaut aot as testImpl
    
    use jdk http client in impl

--- security-aot/build.gradle.kts
+++ security-aot/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     compileOnly(platform(mn.micronaut.core.bom))
     implementation(projects.micronautSecurityOauth2)
     implementation(projects.micronautSecurityJwt)
+    implementation(mn.micronaut.http.client.jdk)
     testImplementation(platform(mn.micronaut.core.bom))
     testImplementation(mn.micronaut.http.server.netty)
     testImplementation(mnTest.micronaut.test.spock)
