7a7c4daf2454d9a2c0c8f24e137c9a42b56f75bf
Sergio del Amo <sergio.delamo@softamo.com>
2024-09-09 12:18:58 +0200

    fix: duplicate JwksSignature beans (#1789)
    * test: reproducer of Issue 1175
    
    See: https://github.com/micronaut-projects/micronaut-security/issues/1775
    
    * don’t instantiate ReactiveJwksSignature in factory
    
    Close: https://github.com/micronaut-projects/micronaut-security/issues/1775

--- security-oauth2/src/main/java/io/micronaut/security/oauth2/client/JwksUriSignatureFactory.java
+++ security-oauth2/src/main/java/io/micronaut/security/oauth2/client/JwksUriSignatureFactory.java
@@ -15,15 +15,12 @@
  */
 package io.micronaut.security.oauth2.client;
 
-import com.nimbusds.jose.jwk.JWKSet;
 import io.micronaut.context.BeanProvider;
 import io.micronaut.context.annotation.EachBean;
 import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Parameter;
 import io.micronaut.core.annotation.Internal;
 import io.micronaut.security.token.jwt.nimbus.ReactiveJwksSignature;
-import io.micronaut.security.token.jwt.signature.jwks.JwkSetFetcher;
-import io.micronaut.security.token.jwt.signature.jwks.JwkValidator;
 import io.micronaut.security.token.jwt.signature.jwks.JwksSignatureConfiguration;
 import io.micronaut.security.token.jwt.signature.jwks.JwksSignatureConfigurationProperties;
 
@@ -36,19 +33,6 @@ import io.micronaut.security.token.jwt.signature.jwks.JwksSignatureConfiguration
 @Factory
 @Internal
 public class JwksUriSignatureFactory {
-    /**
-     * @param jwksSignatureConfiguration JWKS Configuration
-     * @param jwkValidator JWK Validator
-     * @param jwkSetFetcher Json Web Key Set Fetcher
-     * @return a {@link ReactiveJwksSignature} for the {@link JwksSignatureConfiguration}
-     */
-    @EachBean(JwksSignatureConfiguration.class)
-    public ReactiveJwksSignature createJwksUriSignature(@Parameter JwksSignatureConfiguration jwksSignatureConfiguration,
-                                                        JwkValidator jwkValidator,
-                                                        JwkSetFetcher<JWKSet> jwkSetFetcher) {
-        return new ReactiveJwksSignature(jwksSignatureConfiguration, jwkValidator, jwkSetFetcher);
-    }
-
      /**
      * @param openIdProviderMetadata The open id provider metadata
       * @return a {@link JwksSignatureConfiguration} pointed to the jwks_uri exposed via OpenID configuration
---
+++ security-oauth2/src/test/java/io/micronaut/security/oauth2/BeansOfTypeReactiveSignatureConfigurationTest.java
@@ -0,0 +1,24 @@
+package io.micronaut.security.oauth2;
+
+import com.nimbusds.jwt.SignedJWT;
+import io.micronaut.context.annotation.Property;
+import io.micronaut.security.token.jwt.signature.ReactiveSignatureConfiguration;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Property(name = "micronaut.security.token.jwt.signatures.jwks.google.url", value = "https://www.googleapis.com/oauth2/v3/certs")
+@MicronautTest(startApplication = false)
+class BeansOfTypeReactiveSignatureConfigurationTest {
+    @Inject
+    List<ReactiveSignatureConfiguration<SignedJWT>> signatures;
+
+    @Test
+    void testThereIsOnlyOneBeanOfTypeReactiveSignatureConfiguration() {
+        assertEquals(1, signatures.size());
+    }
+}
