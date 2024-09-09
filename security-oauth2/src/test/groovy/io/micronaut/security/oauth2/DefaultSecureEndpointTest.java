ae9f72ab08ad88899f64bda21cbd360c2ce469bf
Sergio del Amo <sergio.delamo@softamo.com>
2024-09-09 13:49:19 +0200

    add more tests

--- security-oauth2/src/test/groovy/io/micronaut/security/oauth2/DefaultSecureEndpointTest.java
+++ security-oauth2/src/test/groovy/io/micronaut/security/oauth2/DefaultSecureEndpointTest.java
@@ -1,6 +1,8 @@
 package io.micronaut.security.oauth2;
 
+import io.micronaut.security.oauth2.configuration.endpoints.SecureEndpointConfiguration;
 import io.micronaut.security.oauth2.endpoint.AuthenticationMethod;
+import io.micronaut.security.oauth2.endpoint.AuthenticationMethods;
 import io.micronaut.security.oauth2.endpoint.DefaultSecureEndpoint;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +10,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import static io.micronaut.security.oauth2.endpoint.AuthenticationMethods.CLIENT_SECRET_BASIC;
+import static io.micronaut.security.oauth2.endpoint.AuthenticationMethods.CLIENT_SECRET_POST;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DefaultSecureEndpointTest {
@@ -24,6 +28,44 @@ class DefaultSecureEndpointTest {
         Set<String> authenticationMethodsSupported = endpoint.getAuthenticationMethodsSupported();
         assertNotNull(authenticationMethodsSupported);
         assertEquals( METHODS, authenticationMethodsSupported);
+
+        endpoint = new DefaultSecureEndpoint(new SecureEndpointConfiguration() {
+            @Override
+            public Optional<AuthenticationMethod> getAuthMethod() {
+                return Optional.of(AuthenticationMethod.CLIENT_SECRET_POST);
+            }
+
+            @Override
+            public Optional<String> getAuthenticationMethod() {
+                return Optional.of(CLIENT_SECRET_POST);
+            }
+
+            @Override
+            public Optional<String> getUrl() {
+                return Optional.of("http://localhost");
+            }
+        }, CLIENT_SECRET_BASIC);
+        assertEquals("http://localhost", endpoint.getUrl());
+        assertEquals(Set.of(CLIENT_SECRET_POST), endpoint.getAuthenticationMethodsSupported());
+
+        endpoint = new DefaultSecureEndpoint(new SecureEndpointConfiguration() {
+            @Override
+            public Optional<AuthenticationMethod> getAuthMethod() {
+                return Optional.empty();
+            }
+
+            @Override
+            public Optional<String> getAuthenticationMethod() {
+                return Optional.empty();
+            }
+
+            @Override
+            public Optional<String> getUrl() {
+                return Optional.of("http://localhost");
+            }
+        }, CLIENT_SECRET_BASIC);
+        assertEquals("http://localhost", endpoint.getUrl());
+        assertEquals(Set.of(CLIENT_SECRET_BASIC), endpoint.getAuthenticationMethodsSupported());
     }
 
     @Deprecated(forRemoval = true)
