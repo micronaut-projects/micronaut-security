c10007c6df4e6204fa44abfe82194a3796fdfd4c
Sergio del Amo <sergio.delamo@softamo.com>
2024-10-15 09:36:42 +0200

    fewer dependencies

--- test-suite-jwks-cache/build.gradle.kts
+++ test-suite-jwks-cache/build.gradle.kts
@@ -5,12 +5,9 @@ plugins {
 
 dependencies {
     testAnnotationProcessor(mn.micronaut.inject.java)
-    testImplementation(mn.micronaut.http)
-    testImplementation(mn.micronaut.json.core)
     testImplementation(mn.micronaut.http.client)
     testImplementation(mn.micronaut.management)
     testImplementation(mn.micronaut.http.server.netty)
-    testImplementation(projects.micronautSecurity)
     testImplementation(projects.micronautSecurityJwt)
     testImplementation(mnCache.micronaut.cache.caffeine)
     testAnnotationProcessor(mnSerde.micronaut.serde.processor)
@@ -18,7 +15,6 @@ dependencies {
     testImplementation(projects.testSuiteUtilsSecurity)
 
     testRuntimeOnly(mnLogging.logback.classic)
-    testAnnotationProcessor(mn.micronaut.inject.java)
     testImplementation(mnTest.micronaut.test.junit5)
     testRuntimeOnly(libs.junit.jupiter.engine)
 }
