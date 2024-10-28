c1bff4fccf25d2c71c6ddea0dd668de49b4d8cff
Sergio del Amo <sergio.delamo@softamo.com>
2024-10-28 16:09:56 +0100

    @Require beans of OauthControllers
    Close: https://github.com/micronaut-projects/micronaut-security/issues/1828

--- security-oauth2/src/main/java/io/micronaut/security/oauth2/routes/DefaultOauthController.java
+++ security-oauth2/src/main/java/io/micronaut/security/oauth2/routes/DefaultOauthController.java
@@ -49,7 +49,7 @@ import reactor.core.publisher.Flux;
  * @author James Kleeh
  * @since 1.2.0
  */
-@Requires(beans = RedirectingLoginHandler.class)
+@Requires(beans = {RedirectingLoginHandler.class, HttpHostResolver.class, HttpLocaleResolver.class})
 @EachBean(OauthClient.class)
 public class DefaultOauthController implements OauthController {
 
