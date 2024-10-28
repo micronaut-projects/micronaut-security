12006b875261da7f2ac2e7448ef38117b12ae3f7
Sergio del Amo <sergio.delamo@softamo.com>
2024-10-28 17:06:02 +0100

    doc: @RolesAllowed and @Secured in the same method
    see: https://github.com/micronaut-projects/micronaut-security/issues/28

--- security/src/test/groovy/io/micronaut/security/rolesallowed/RolesAllowedSpec.groovy
+++ security/src/test/groovy/io/micronaut/security/rolesallowed/RolesAllowedSpec.groovy
@@ -3,12 +3,16 @@ package io.micronaut.security.rolesallowed
 import io.micronaut.context.annotation.Requires
 import io.micronaut.context.env.Environment
 import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Status
 import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.security.MockAuthenticationProvider
 import io.micronaut.security.SuccessAuthenticationScenario
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule
 import io.micronaut.security.testutils.EmbeddedServerSpecification
 import jakarta.inject.Singleton
 
@@ -63,12 +67,43 @@ class RolesAllowedSpec extends EmbeddedServerSpecification {
         e.response.status() == HttpStatus.FORBIDDEN
     }
 
+    def "Both @Secured and @RolesAllowed should not be used in the same method"() {
+        when:
+        HttpResponse<?> response = client.exchange(HttpRequest.GET("/both/securedfirstrolesallowedsecond").basicAuth("sherlock", "password"))
+
+        then:
+        noExceptionThrown()
+        HttpStatus.ACCEPTED == response.status()
+
+        when: // @Secured is used not @RolesAllowed
+        response = client.exchange(HttpRequest.GET("/both/securedfirstrolesallowedsecond").basicAuth("user", "password"))
+
+        then:
+        noExceptionThrown()
+
+        HttpStatus.ACCEPTED == response.status()
+
+        when:
+        response = client.exchange(HttpRequest.GET("/both/rolesallowedfirstsecuredsecond").basicAuth("sherlock", "password"))
+
+        then:
+        noExceptionThrown()
+        HttpStatus.ACCEPTED == response.status()
+
+        when: // @RolesAllowed is used not @Secured
+        response = client.exchange(HttpRequest.GET("/both/rolesallowedfirstsecuredsecond").basicAuth("user", "password"))
+
+        then:
+        HttpClientResponseException ex = thrown()
+        HttpStatus.FORBIDDEN == ex.status
+    }
+
     @Singleton
     @Requires(env = Environment.TEST)
     @Requires(property = 'spec.name', value = 'RolesAllowedSpec')
     static class AuthenticationProviderUserPassword extends MockAuthenticationProvider {
         AuthenticationProviderUserPassword() {
-            super([new SuccessAuthenticationScenario('user', ['ROLE_USER'])])
+            super([new SuccessAuthenticationScenario('user', ['ROLE_USER']), new SuccessAuthenticationScenario('sherlock', ['ROLE_DETECTIVE'])])
         }
     }
 
@@ -97,4 +132,20 @@ class RolesAllowedSpec extends EmbeddedServerSpecification {
     }
 
 
+    @Controller("/both")
+    static class SecuredAndRolesAllowed {
+        @Status(HttpStatus.ACCEPTED)
+        @Get("/securedfirstrolesallowedsecond")
+        @Secured(SecurityRule.IS_AUTHENTICATED)
+        @RolesAllowed("ROLE_DETECTIVE")
+        void securedfirstrolesallowedsecond() {
+        }
+
+        @Status(HttpStatus.ACCEPTED)
+        @Get("/rolesallowedfirstsecuredsecond")
+        @RolesAllowed("ROLE_DETECTIVE")
+        @Secured(SecurityRule.IS_AUTHENTICATED)
+        void rolesallowedfirstsecuredsecond() {
+        }
+    }
 }
