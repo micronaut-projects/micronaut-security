38f2fb974f3f6e6d150e6bacf0093776b6ec7ca3
Sergio del Amo <sergio.delamo@softamo.com>
2025-06-16 17:41:42 +0200

    micronaut.server.port=-1

--- test-suite-aot-authserver/src/test/java/io/micronaut/security/test/aot/authserver/a/HealthTest.java
+++ test-suite-aot-authserver/src/test/java/io/micronaut/security/test/aot/authserver/a/HealthTest.java
@@ -1,5 +1,6 @@
 package io.micronaut.security.test.aot.authserver.a;
 
+import io.micronaut.context.annotation.Property;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
@@ -15,6 +16,7 @@ import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Property(name = "micronaut.server.port", value = "-1")
 @MicronautTest
 class HealthTest {
 
