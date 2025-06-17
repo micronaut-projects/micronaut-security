09b5ba58fb091ae4e4caf1bbdf0f662f68dbb9b7
Sergio del Amo <sergio.delamo@softamo.com>
2025-06-17 09:30:00 +0200

    Unsupported POST Content type status configurable (#1976)
    * ci: projectVersion=4.13.0-SNAPSHOT [ci skip]
    
    * unsupported post content type status configurable
    
    * Update gradle.properties
    
    * add log statements
    
    * use int instead of http status
    
    * remove unused imports

--- security/src/main/java/io/micronaut/security/endpoints/OauthController.java
+++ security/src/main/java/io/micronaut/security/endpoints/OauthController.java
@@ -45,6 +45,8 @@ import java.util.Optional;
 
 import jakarta.inject.Inject;
 import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 
 import static io.micronaut.security.endpoints.TokenRefreshRequest.GRANT_TYPE;
@@ -66,6 +68,7 @@ import static io.micronaut.security.endpoints.TokenRefreshRequest.GRANT_TYPE;
 @Controller("${" + OauthControllerConfigurationProperties.PREFIX + ".path:/oauth/access_token}")
 @Secured(SecurityRule.IS_ANONYMOUS)
 public class OauthController {
+    private static final Logger LOG = LoggerFactory.getLogger(OauthController.class);
 
     private final RefreshTokenPersistence refreshTokenPersistence;
     private final RefreshTokenValidator refreshTokenValidator;
@@ -121,7 +124,12 @@ public class OauthController {
                                                    @Nullable @CookieValue("JWT_REFRESH_TOKEN") String cookieRefreshToken) {
         Optional<MediaType> contentTypeOptional = request.getContentType();
         if (!(contentTypeOptional.isPresent() && oauthControllerConfiguration.getPostContentTypes().contains(contentTypeOptional.get().getName()))) {
-            return Publishers.just(HttpResponse.notFound());
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Unsupported content type {}. OAuth Controller supports: {}",
+                    contentTypeOptional.map(MediaType::getName).orElse(""),
+                    String.join(",", oauthControllerConfiguration.getPostContentTypes()));
+            }
+            return Publishers.just(HttpResponse.status(HttpStatus.valueOf(oauthControllerConfiguration.getUnsupportedPostContentTypeStatus())));
         }
         TokenRefreshRequest tokenRefreshRequest = body == null ? null :
             new TokenRefreshRequest(body.get(GRANT_TYPE), body.get(TokenRefreshRequest.GRANT_TYPE_REFRESH_TOKEN));
