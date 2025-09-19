a0774e85b7dbb832774710ec5dbbcc19bbd3ec7b
Sergio del Amo <sergio.delamo@softamo.com>
2025-09-19 18:07:12 +0200

    security: don’t log token (#2026)

--- security/src/test/groovy/io/micronaut/security/token/reader/DefaultTokenResolverSpec.groovy
+++ security/src/test/groovy/io/micronaut/security/token/reader/DefaultTokenResolverSpec.groovy
@@ -1,15 +1,25 @@
 package io.micronaut.security.token.reader
 
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.AppenderBase
 import io.micronaut.context.annotation.Property
 import io.micronaut.context.annotation.Requires
 import io.micronaut.core.order.Ordered
+import io.micronaut.core.util.CollectionUtils
 import io.micronaut.http.HttpHeaders
 import io.micronaut.http.HttpMethod
 import io.micronaut.http.simple.SimpleHttpRequest
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
+import org.slf4j.LoggerFactory
 import spock.lang.Specification
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.AppenderBase
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.LinkedBlockingQueue
 
 @Property(name = "spec.name", value = "DefaultTokenResolverSpec")
 @MicronautTest
@@ -20,13 +30,34 @@ class DefaultTokenResolverSpec extends Specification {
 
     void "Beans of type TokenReader are evaluated in order"() {
         given:
-        SimpleHttpRequest request = new SimpleHttpRequest(HttpMethod.POST, "/analytics/report", null)
-        request.headers.add("X-API-KEY", "validxxx")
+        MemoryAppender appender = new MemoryAppender()
+        Logger logger = (Logger) LoggerFactory.getLogger(DefaultTokenResolver.class)
+        logger.addAppender(appender)
+        logger.setLevel(Level.TRACE)
+        appender.start()
 
-        expect:
-        defaultTokenResolver.resolveToken(request).isPresent()
-        "validxxx" == defaultTokenResolver.resolveToken(request).get()
-        ["validxxx"] == defaultTokenResolver.resolveTokens(request)
+        SimpleHttpRequest request = new SimpleHttpRequest(HttpMethod.POST, "/analytics/report", null)
+        String apiKey = "validxxx"
+        request.headers.add("X-API-KEY", apiKey)
+
+        when:
+        Optional<String> tokenOptional = defaultTokenResolver.resolveToken(request)
+
+        then:
+        tokenOptional.isPresent()
+        apiKey == tokenOptional.get()
+
+        and: 'token is not logged'
+        CollectionUtils.isNotEmpty(appender.events)
+        1 == appender.events.size()
+        appender.events.stream().noneMatch(e -> e.contains(apiKey))
+        appender.events.stream().anyMatch(e -> e == "Token found in request POST /analytics/report")
+
+        when:
+        List<String> tokens = defaultTokenResolver.resolveTokens(request)
+
+        then:
+        [apiKey] == tokens
 
         when:
         request = new SimpleHttpRequest(HttpMethod.POST, "/analytics/report", null)
@@ -63,6 +94,15 @@ class DefaultTokenResolverSpec extends Specification {
         @Override
         int getOrder() {
             return Ordered.HIGHEST_PRECEDENCE;
+        }
+    }
+
+    static class MemoryAppender extends AppenderBase<ILoggingEvent> {
+        final BlockingQueue<String> events = new LinkedBlockingQueue<>()
+
+        @Override
+        protected void append(ILoggingEvent e) {
+            events.add(e.formattedMessage)
         }
     }
 }
