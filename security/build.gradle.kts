8716297f23c8ff0522a109e18f5fdb7d28784cf7
araysse <raysse.abdessamad@gmail.com>
2025-10-22 13:49:53 +0100

    add languageVersion 21

--- security/build.gradle.kts
+++ security/build.gradle.kts
@@ -49,4 +49,7 @@ dependencies {
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
