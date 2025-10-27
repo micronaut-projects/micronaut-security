cd5a2faf8399ea4e8e9e4317b70b346779436400
Sergio del Amo <sergio.delamo@softamo.com>
2025-10-27 12:57:52 +0100

    test: remove junit.framework.TestCase

--- security/src/test/java/io/micronaut/security/token/ClaimsUtilsTest.java
+++ security/src/test/java/io/micronaut/security/token/ClaimsUtilsTest.java
@@ -1,15 +1,16 @@
 package io.micronaut.security.token;
 
-import junit.framework.TestCase;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-public class ClaimsUtilsTest extends TestCase {
+class ClaimsUtilsTest {
 
     static Stream<Arguments> paramsProvider() {
         return Stream.of(
