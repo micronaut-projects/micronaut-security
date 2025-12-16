ce6ccb5b4f42eaf43cf58a130a6ba98cf12fbec9
Sergio del Amo <sergio.delamo@softamo.com>
2025-12-16 11:49:42 +0100

    remove junit-platform-engine

--- test-suite-jwt-nimbus/build.gradle.kts
+++ test-suite-jwt-nimbus/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 dependencies {
     testImplementation(platform(mnTest.micronaut.test.bom))
-    testImplementation(libs.junit.platform.engine)
+    testImplementation(mnTest.junit.platform.engine)
     testImplementation(mnTest.junit.jupiter.engine)
     testImplementation(projects.testSuiteJwtTck)
 }
