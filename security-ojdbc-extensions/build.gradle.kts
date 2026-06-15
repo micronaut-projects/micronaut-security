4c5dc16f0df9c9a751df65414c313bfe41de641a
Sergio del Amo <sergio.del.amo.caballero@oracle.com>
2026-06-15 17:16:11 +0200

    remove unused dependencies

--- security-ojdbc-extensions/build.gradle.kts
+++ security-ojdbc-extensions/build.gradle.kts
@@ -28,35 +28,11 @@ dependencies {
     implementation(mn.micronaut.http.client.core)
     implementation(mnReactor.micronaut.reactor)
     implementation(projects.micronautSecurityOauth2)
-
     testAnnotationProcessor(mn.micronaut.inject.java)
-
     testImplementation(projects.micronautSecurityJwt)
     testImplementation(libs.managed.nimbus.jose.jwt)
-    testImplementation(mn.micronaut.http.client)
-
-    // HTTP Server
     testImplementation(mn.micronaut.http.server.netty)
-
-    // HTTP Client
     testImplementation(mn.micronaut.http.client)
-
-    // Data
-    testAnnotationProcessor(mnData.micronaut.data.processor)
-    testImplementation(mnData.micronaut.data.jdbc)
-
-    // Database migration
-    testImplementation(mnLiquibase.micronaut.liquibase)
-
-    // Connection Pool
-    testImplementation(mnSql.micronaut.jdbc.hikari)
-
-    // Serialization
     annotationProcessor(mnSerde.micronaut.serde.processor)
     implementation(mnSerde.micronaut.serde.jackson)
-
-    testImplementation(mn.micronaut.http)
-
-    testImplementation(platform(mnTest.boms.testcontainers))
-    testImplementation(libs.testcontainers.oracle.free)
 }
