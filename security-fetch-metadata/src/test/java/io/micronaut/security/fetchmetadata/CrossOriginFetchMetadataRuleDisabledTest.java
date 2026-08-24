d35f1b86096d62642d3de67e58b119db62ba7b7c
Sergio del Amo <sergio.del.amo.caballero@oracle.com>
2026-08-24 11:47:55 +0200

    remove used imports

--- security-fetch-metadata/src/test/java/io/micronaut/security/fetchmetadata/CrossOriginFetchMetadataRuleDisabledTest.java
+++ security-fetch-metadata/src/test/java/io/micronaut/security/fetchmetadata/CrossOriginFetchMetadataRuleDisabledTest.java
@@ -29,7 +29,6 @@ import io.micronaut.http.client.annotation.Client;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.security.fetchmetadata.rules.FetchMetadataRulesConfigurationProperties;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
--- security-fetch-metadata/src/test/java/io/micronaut/security/fetchmetadata/NoFetchMetadataDefaultTest.java
+++ security-fetch-metadata/src/test/java/io/micronaut/security/fetchmetadata/NoFetchMetadataDefaultTest.java
@@ -21,7 +21,6 @@ import io.micronaut.http.client.BlockingHttpClient;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
